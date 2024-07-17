2c91017eb30cdf89d76bf629f0aa6df0588f270c
Lexuil <ojumanab@unal.edu.co>
2024-07-16 20:53:26 -0500

    Add simple template variables identification

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,47 @@
+<script setup lang="ts">
+import { ref, watch } from "vue"
+
+const editor = ref('')
+const variables = ref<string[]>([])
+
+const evaluateRegex = /\{\{([\s\S]+?)\}\}/g
+
+watch(editor, (text) => {
+  variables.value = [
+    ...text.matchAll(evaluateRegex)
+  ].map(match => match[1].slice(4))
+})
+</script>
+
 <template>
-  <main class="flex items-center justify-center bg-black min-h-screen">
+  <main class="flex flex-col gap-5 items-center justify-center bg-black min-h-screen">
     <h1 class="text-4xl font-bold text-white">
-      Hello world
+      doT.js Editor
     </h1>
+
+    <textarea
+      id="editor"
+      v-model="editor"
+      name="editor"
+      cols="39"
+      rows="10"
+      class="bg-gray-800 text-white p-2 mt-4"
+    />
+
+    <div class="flex flex-col gap-2">
+      <h2 class="text-2xl font-bold text-white">
+        Variables
+      </h2>
+
+      <ul class="flex flex-col gap-1">
+        <li
+          v-for="variable in variables"
+          :key="variable"
+          class="text-white"
+        >
+          {{ variable }}
+        </li>
+      </ul>
+    </div>
   </main>
 </template>
