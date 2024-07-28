58c433c3f12b7ff1e5b5e329ba54abfe355bdc40
Lexuil <ojumanab@unal.edu.co>
2024-07-28 12:19:22 -0500

    Add EditorContainer component

---
+++ src/components/EditorContainer.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import useEditor from '@/composables/useEditor'
+useEditor('container')
+</script>
+
+<template>
+  <div
+    id="container"
+    class="h-96 w-screen md:w-[43rem]"
+  />
+</template>
